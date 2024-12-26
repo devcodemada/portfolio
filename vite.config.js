a1588d898e4a56b99ab5fd5d3721db3ec1c7c900
Devcodemada <devcodemada@gmail.com>
2024-12-26 09:31:23 +0700

    bugg

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import jsconfigPaths from 'vite-jsconfig-paths';
+import rehypePrism from '@mapbox/rehype-prism';
 import mdx from '@mdx-js/rollup';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { vitePlugin as remix } from '@remix-run/dev';
 import rehypeImgSize from 'rehype-img-size';
 import rehypeSlug from 'rehype-slug';
-import rehypePrism from '@mapbox/rehype-prism';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { defineConfig } from 'vite';
+import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
@@ -25,7 +22,7 @@ export default defineConfig({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
       providerImportSource: '@mdx-js/react',
     }),
-    remixCloudflareDevProxy(),
+    // remixCloudflareDevProxy(),
     remix({
       routes(defineRoutes) {
         return defineRoutes(route => {
